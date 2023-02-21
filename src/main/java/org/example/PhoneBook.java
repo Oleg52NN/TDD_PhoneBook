58a25a5f5728287a6976f3c27e23ee07240623e5
Oleg NN <zvoprosno@gmail.con>
2023-02-22 02:01:25 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,7 @@
 package org.example;
 
 public class PhoneBook {
+    public static int add(String name, String phone){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,24 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+
+import static org.example.PhoneBook.add;
+
 public class PhoneBookTest {
+    @Test
+    public void addTestOne() {
+        int expected = add("Катя", "+7-944-333-22-11");
+        expected++;
+        Assert.assertEquals(expected, add("Федя", "+7-955-333-22-11"));
+    }
+    @Test
+    public void addTestTwo(){
+        int expected = add("Катя", "+7-944-333-22-11");
+        add("Федя", "+7-955-333-22-11");
+        add("Катя", "+7-977-333-22-11");
+        expected++;
+        Assert.assertEquals(expected, add("Федя", "+7-955-333-22-11"));
+
+    }
+
 }
