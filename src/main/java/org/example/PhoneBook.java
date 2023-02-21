c0a8f6cfce0d90cf97fc51599e935c2d335ff505
Oleg NN <zvoprosno@gmail.con>
2023-02-22 02:07:35 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+    public static Map<String,String> phoneBook = new TreeMap<>();
     public static int add(String name, String phone){
-        return 0;
+        phoneBook.put(name, phone);
+        return phoneBook.size();
     }
 }
